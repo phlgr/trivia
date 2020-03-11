2ea9cd8b5c9bd01bdbe3b0436ad138a8ce429d2c
phlgr <philippm.gartz@gmail.com>
2020-03-11 16:42:02 +0100

    Add Router

--- src/App.js
+++ src/App.js
@@ -1,14 +1,37 @@
 import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 import "./App.css";
 import Header from "./components/Header/Header";
+import Add from "./pages/Add";
+import Vote from "./pages/Vote";
+import Result from "./pages/Result";
 
-function App() {
+export default function App() {
   return (
-    <div>
-      <Header />
-    </div>
+    <>
+      <Router>
+        <main>
+          <Header />
+          <Switch>
+            <Route exact path="/">
+              Hello
+            </Route>
+
+            <Route path="/add">
+              <Add />
+            </Route>
+
+            <Route path="/vote">
+              <Vote />
+            </Route>
+
+            <Route path="/result">
+              <Result />
+            </Route>
+          </Switch>
+        </main>
+      </Router>
+    </>
   );
 }
-
-export default App;
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,12 +1,15 @@
 import React from "react";
 import "./Header.css";
 import Logo from "./logo.svg";
+import { Link } from "react-router-dom";
 
 function Header() {
   return (
     <header>
       <div className="brand">
-        <img className="logo" src={Logo} alt=""></img>
+        <Link to="/">
+          <img className="logo" src={Logo} alt=""></img>
+        </Link>
         <div className="name"> SimplePoll</div>
       </div>
     </header>
