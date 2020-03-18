28afec1a01b5aac31099c9d5b31ef91cc09f17c0
phlgr <philippm.gartz@gmail.com>
2020-03-18 18:50:16 +0100

    Add Emotion to Button Component

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,6 +1,15 @@
-import React from 'react';
-import './Button.css';
+import React from "react";
+import "./Button.css";
+import styled from "@emotion/styled";
+
+const ButtonPrimary = styled.button`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  height: 40vh;
+`;
 
 export default function Button(props) {
-  return <button className="btn-primary" {...props} />;
+  return <ButtonPrimary {...props} />;
 }
